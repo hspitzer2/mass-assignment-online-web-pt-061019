ad11afe1a9c582220fca433b4c559aef3cfd304b
Hillary Spitzer <hspitzer2@gmail.com>
2019-07-24 14:56:49 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,10 +4,8 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
 
     def initialize(attributes)
-      # binding.pry
-      # attributes = []
-      # attributes << self
+    
         attributes.map {|key, value| self.send(("#{key}="), value)}
-      # attributes.map {|key, value| self.send(("#{key}="), value)}
+    
     end
 end
